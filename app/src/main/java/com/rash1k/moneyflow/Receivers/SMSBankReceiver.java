eefe01b248c15589eda3aacec930056c44b92fd0
Dmitriy Ostrovskiy <rash1k-1@mail.ru>
2016-07-08 20:14:30 +0300

    08.07.2016 20:14

--- app/src/main/java/com/rash1k/moneyflow/Receivers/SMSBankReceiver.java
+++ app/src/main/java/com/rash1k/moneyflow/Receivers/SMSBankReceiver.java
@@ -1,13 +1,13 @@
-package com.rash1k.moneyflow.Receivers;
+package com.rash1k.moneyflow.receivers;
 
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.widget.Toast;
 
-public class SMSBankReceiver extends BroadcastReceiver {
+public class SmsBankReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        Toast.makeText(context, "SMS Received", Toast.LENGTH_LONG);
+        Toast.makeText(context, "SMS Received", Toast.LENGTH_LONG).show();
     }
 }
